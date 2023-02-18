7e5cc7c1ea9ed6b79a87043ccce5ae8c1cf1098f
Drunken Whaler <33fere11@gmail.com>
2023-02-18 13:17:48 +0200

    remove from converter.py input

--- modules/convertor.py
+++ modules/convertor.py
@@ -2,18 +2,5 @@ from bases import Bases
 bases = Bases()
 
 
-def base_type(string: str):
-
-    while True:
-        try:
-            input_base = int(input(f"{string} base type(2-36): "))
-            if input_base not in range(2, 17):
-                print("Basenumber is incorrect.")
-                continue
-            return input_base
-        except ValueError:
-            print("Basenumber is incorrect.")
-
-
 def convertor(base: int, number: int):
     return bases.toBase(number, base)
--- modules/inputs.py
+++ modules/inputs.py
@@ -1,3 +1,16 @@
+def base_type(string: str):
+
+    while True:
+        try:
+            input_base = int(input(f"{string} base type(2-36): "))
+            if input_base not in range(2, 17):
+                print("Basenumber is incorrect.")
+                continue
+            return input_base
+        except ValueError:
+            print("Basenumber is incorrect.")
+
+
 def input_int_number(input_base: int, string: str):
     while True:
         try:
